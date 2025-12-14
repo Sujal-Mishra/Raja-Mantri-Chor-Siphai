ceb5ef5ec430446f73c8ee61982e8ccb628842f7
Sujal Mishra <90392616+Sujal-Mishra@users.noreply.github.com>
2025-12-14 12:42:44 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const roomRoutes = require("./routes/room.routes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/room", roomRoutes);
+
+app.listen(3000, () => {
+  console.log("🚀 Server running on port 3000");
+});
